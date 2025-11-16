e0a8e42452b1de9e93b37d83f8dc9a5ddf002daf
Zwei <yapzhenwei13@gmail.com>
2025-11-16 12:38:07 +0800

    Add files via upload

--- Main/obj/Debug/net9.0/Main.AssemblyInfo.cs
+++ Main/obj/Debug/net9.0/Main.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Main")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+509369d992ddf2a8174b77a90df34fc90439d606")]
 [assembly: System.Reflection.AssemblyProductAttribute("Main")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Main")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
