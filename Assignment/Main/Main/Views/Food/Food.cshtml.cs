d77d4ac02663364b8cbcabdd9abccecf830d011b
Zwei <yapzhenwei13@gmail.com>
2025-12-11 02:39:32 +0800

    1

---
+++ Assignment/Main/Main/Views/Food/Food.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Main.Views.Food
+{
+    public class FoodModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
