26717afcf0decc72847b1b9dbc2524fae72e7a28
Zwei <yapzhenwei13@gmail.com>
2025-12-12 02:17:19 +0800

    Latest

--- Assignment/Main/Main/QRCodeHelper.cs
+++ Assignment/Main/Main/QRCodeHelper.cs
@@ -1,7 +1,6 @@
 ﻿using QRCoder;
-using SixLabors.ImageSharp.Formats;
-using System.Drawing;
-using System.Drawing.Imaging;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
 
 namespace Main;
 
@@ -11,13 +10,10 @@ public class QRCodeHelper
     {
         using var qrGenerator = new QRCodeGenerator();
         using var qrCodeData = qrGenerator.CreateQrCode(data, QRCodeGenerator.ECCLevel.Q);
-        using var qrCode = new QRCode(qrCodeData);
-        using var qrCodeImage = qrCode.GetGraphic(20);
+        using var qrCode = new PngByteQRCode(qrCodeData);
 
-        using var ms = new MemoryStream();
-        qrCodeImage.Save(ms, ImageFormat.Png);
-        byte[] byteImage = ms.ToArray();
-        return Convert.ToBase64String(byteImage);
+        byte[] qrCodeBytes = qrCode.GetGraphic(20);
+        return Convert.ToBase64String(qrCodeBytes);
     }
 
     public static string GenerateRedemptionCode()
